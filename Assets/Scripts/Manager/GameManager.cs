3b3bcff298557f243b82e333ce3baaf3b28c7022
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-08-29 14:14:51 +0900

    Update GameManager.cs

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using Unity.VisualScripting;
 using UnityEngine;
 public enum GameState
 {
@@ -9,7 +12,30 @@ public enum GameState
     Select,
     End
 }
+
 public class GameManager : Singleton<GameManager>
 {
-    
+    public Action startAction;
+    public GameState gameState = GameState.Start;
+    void Update()
+    {
+        switch (gameState)
+        {
+            case GameState.Start:
+                startAction();
+                gameState = GameState.Running;
+                break;
+            case GameState.Running:
+                break;
+            case GameState.Minigame:
+                break;
+            case GameState.Select:
+                break;
+            case GameState.End:
+                break;
+        }
+        
+
+    }
+
 }
