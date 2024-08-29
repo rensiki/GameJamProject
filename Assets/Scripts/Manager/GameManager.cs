68157b1ffcc319262dd11240256cf051bc8f31a5
highlow12 <77542378+highlow12@users.noreply.github.com>
2024-08-29 14:00:52 +0900

    Make GameState enum

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,7 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum GameState
+{
+    Start,
+    Running,
+    Minigame,
+    Select,
+    End
+}
 public class GameManager : Singleton<GameManager>
 {
     
