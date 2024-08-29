8045f243cf5f472d3747dc74608e3aa6266d1e76
KimTaeMin <ktm051113@naver.com>
2024-08-29 16:01:53 +0900

    arrow Rotation

--- Assets/Scripts/MiniGame2/ShotingScript.cs
+++ Assets/Scripts/MiniGame2/ShotingScript.cs
@@ -7,14 +7,14 @@ public class ShotingScript : MonoBehaviour
     public GameObject ball;
     public GameObject arrow;
     public float ballSpeed = 1f;
-    public float rotateSpeed = 1f;
+    public float rotateSpeed = 100000f;
     
     Vector3 startPos;
     Vector3 endPos;
     Vector3 direction;
     Transform arrowTrans;
 
-
+    float angle;
     
     bool isDragging = false;
 
@@ -27,10 +27,12 @@ public class ShotingScript : MonoBehaviour
     void Update()
     {
         if(isDragging){
-            direction = endPos-startPos;
+            direction = Camera.main.ScreenToWorldPoint(Input.mousePosition)-startPos;
             DragArrow();
         }
         DragFunction();
+        //ray
+        Debug.DrawRay(transform.position, direction*-1, Color.red);
     }
 
     void DragFunction(){
@@ -48,20 +50,25 @@ public class ShotingScript : MonoBehaviour
     }
     void Shot(){
         GameObject newBall = Instantiate(ball, transform.position, Quaternion.identity);
-        newBall.GetComponent<Rigidbody2D>().AddForce((direction)*ballSpeed*-1, ForceMode2D.Impulse);
+        newBall.GetComponent<Rigidbody2D>().AddForce((endPos-startPos)*ballSpeed*-1, ForceMode2D.Impulse);
         isDragging = false;
     }
 
     void DragArrow(){
         //위에서 구한 목표 방향(Vector3)을 사분위수로 전환하는 메서드
-        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        //Quaternion targetRotation = Quaternion.LookRotation(direction);
 
         //(시작값, 목표값, 회전 속도)를 인자로 받아 회전 값을 연산해주는 메서드
-        Quaternion rotateAmount = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        //Quaternion rotateAmount = Quaternion.RotateTowards(arrowTrans.rotation, targetRotation, rotateSpeed * Time.deltaTime);
 
         //회전값 적용
-        arrowTrans.rotation = rotateAmount;
+        //Debug.Log(rotateAmount);
+        //arrowTrans.rotation = rotateAmount;
         
+        angle = Quaternion.FromToRotation(new Vector3(0,100,0), direction*-1).eulerAngles.z;
+        Debug.Log(angle);
+        arrowTrans.rotation = Quaternion.Euler(0, 0, angle);
+
     }
 
 }
